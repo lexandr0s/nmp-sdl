cd1d76207923a31de970e7569844e4cefaca9b78
Lexandros <lexandros71@gmail.com>
2015-09-06 20:48:36 +0300

    Revert "audioplayer: -add flv format (THX Houdini)"
    This reverts commit 29aa47aef0201dd21102c9452119e2f63c0769e5.
    
    Conflicts:
    	src/driver/file.cpp

--- src/gui/audioplayer.cpp
+++ src/gui/audioplayer.cpp
@@ -202,7 +202,6 @@ void CAudioPlayerGui::Init(void)
 		audiofilefilter.addFilter("ogg");
 		audiofilefilter.addFilter("wav");
 		audiofilefilter.addFilter("flac");
-		audiofilefilter.addFilter("flv");
 #ifdef ENABLE_FFMPEGDEC
 		audiofilefilter.addFilter("aac");
 		audiofilefilter.addFilter("dts");
@@ -1268,7 +1267,6 @@ bool CAudioPlayerGui::openFilebrowser(void)
 					||  (files->getType() == CFile::FILE_AAC)
 #endif
 					||  (files->getType() == CFile::FILE_FLAC)
-					||  (files->getType() == CFile::FILE_FLV)
 			   )
 			{
 				CAudiofileExt audiofile(files->Name,
@@ -1360,7 +1358,6 @@ bool CAudioPlayerGui::openFilebrowser(void)
 											|| fileType == CFile::FILE_OGG
 											|| fileType == CFile::FILE_WAV
 											|| fileType == CFile::FILE_FLAC
-											|| fileType == CFile::FILE_FLV
 									   )
 									{
 										CAudiofileExt audioFile(filename,fileType);
